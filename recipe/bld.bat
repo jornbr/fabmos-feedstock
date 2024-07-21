1692e796bd0c758b36a7713df870e7f4cba7c037
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-21 09:40:39 +0100

    clean-up

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
 echo [build_ext]>"%SRC_DIR%\setup.cfg"
 echo cmake_opts=-G "MinGW Makefiles" %CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
+
+set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
+
 "%PYTHON%" -m pip install . -vv
