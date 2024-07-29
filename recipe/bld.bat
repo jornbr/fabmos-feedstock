9415ca471b3df184e7c6aadb68959012987983cc
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-29 16:22:16 +0100

    customize for local Windows builds using MSVC + Intel Fortran

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-echo [build_ext]>"%SRC_DIR%\setup.cfg"
-echo cmake_opts=-G "MinGW Makefiles" %CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
+REM echo [build_ext]>"%SRC_DIR%\setup.cfg"
+REM echo cmake_opts=-G "MinGW Makefiles" %CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
 
 set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
 
